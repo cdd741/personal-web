1eff8ca759c5f58297eb99dc8bdf63ff41630dfc
dredd <dre.chen1996@gmail.com>
2020-09-29 22:04:35 -0700

    Updates

---
+++ precache-manifest.8e05ab0ed97dec540b36b59a3a1a38fa.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88898724aac3c26ae721cf22cccee17f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "208eca0adde34e3034d1",
+    "url": "/static/css/main.a8ddf8ce.chunk.css"
+  },
+  {
+    "revision": "dfc0844478ea2cb6935d",
+    "url": "/static/js/2.aa60ceb5.chunk.js"
+  },
+  {
+    "revision": "596676716ca1328e2733d607535d6ca7",
+    "url": "/static/js/2.aa60ceb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "208eca0adde34e3034d1",
+    "url": "/static/js/main.440f2dce.chunk.js"
+  },
+  {
+    "revision": "e0ca6d224cc6f7e6205f",
+    "url": "/static/js/runtime-main.de46f35b.js"
+  },
+  {
+    "revision": "6a41eb1d2942c58f1583426c6c2b914d",
+    "url": "/static/media/1.6a41eb1d.jpg"
+  },
+  {
+    "revision": "cdc7e206adefff7d1f6145c37377cad7",
+    "url": "/static/media/10.cdc7e206.jpg"
+  },
+  {
+    "revision": "97a58b00457f1da548ff5cd5c0f24817",
+    "url": "/static/media/11.97a58b00.jpg"
+  },
+  {
+    "revision": "15ef9907db7e4b6e4141a08007516a39",
+    "url": "/static/media/12.15ef9907.jpg"
+  },
+  {
+    "revision": "dc73fd6736756691575f1737bc01f1dd",
+    "url": "/static/media/13.dc73fd67.jpg"
+  },
+  {
+    "revision": "a8bb8e49c47db8edccc830ad6b333155",
+    "url": "/static/media/14.a8bb8e49.jpg"
+  },
+  {
+    "revision": "c767f068a1563fad2b89f514dec73295",
+    "url": "/static/media/15.c767f068.jpg"
+  },
+  {
+    "revision": "650f74c75dd2151dca072ec4ad7ccd21",
+    "url": "/static/media/16.650f74c7.jpg"
+  },
+  {
+    "revision": "c870f0d7c3df104635e721b39a611309",
+    "url": "/static/media/17.c870f0d7.jpg"
+  },
+  {
+    "revision": "325c3eb928317ab3feff8613c3ca75ea",
+    "url": "/static/media/18.325c3eb9.jpg"
+  },
+  {
+    "revision": "4664c20812e4b58039ddca9de2824cad",
+    "url": "/static/media/19.4664c208.jpg"
+  },
+  {
+    "revision": "c1fe3adf1ea11454bb1eb5d714c99351",
+    "url": "/static/media/2.c1fe3adf.jpg"
+  },
+  {
+    "revision": "1a1f08f776735349bc8b5908687f9d6a",
+    "url": "/static/media/20.1a1f08f7.jpg"
+  },
+  {
+    "revision": "5e41f3a344e7c2bfd4d0f4af912634bb",
+    "url": "/static/media/21.5e41f3a3.jpg"
+  },
+  {
+    "revision": "d6b4171e4cb14340cd3fc3e765612fbe",
+    "url": "/static/media/22.d6b4171e.jpg"
+  },
+  {
+    "revision": "0465c11aa801c3f84b97ec58d46a10c5",
+    "url": "/static/media/23.0465c11a.jpg"
+  },
+  {
+    "revision": "22f4e4c895f68d62ae4d6378dac951f9",
+    "url": "/static/media/24.22f4e4c8.jpg"
+  },
+  {
+    "revision": "232d8a5c9f6256e603e19bab9c64c6ec",
+    "url": "/static/media/25.232d8a5c.jpg"
+  },
+  {
+    "revision": "6c9474a4eb8aefb2cd6281f69c9c117d",
+    "url": "/static/media/26.6c9474a4.jpg"
+  },
+  {
+    "revision": "e6059c2d0328c5bc4e9d63d5debd829a",
+    "url": "/static/media/27.e6059c2d.jpg"
+  },
+  {
+    "revision": "03b4b27b9f31f9f307717ae623db856c",
+    "url": "/static/media/28.03b4b27b.jpg"
+  },
+  {
+    "revision": "967e8e749edd8e817223a5377547f310",
+    "url": "/static/media/29.967e8e74.jpg"
+  },
+  {
+    "revision": "45b214936b63a6cb6845164c94befd63",
+    "url": "/static/media/3.45b21493.jpg"
+  },
+  {
+    "revision": "188d313b1a2149108d9dea2a5838690f",
+    "url": "/static/media/30.188d313b.jpg"
+  },
+  {
+    "revision": "c8a29346eaafa57c9e195f00731207c8",
+    "url": "/static/media/33.c8a29346.jpg"
+  },
+  {
+    "revision": "22fb6a19f975a40b665f063dce70fee2",
+    "url": "/static/media/4.22fb6a19.jpg"
+  },
+  {
+    "revision": "8f1bbccd48e470231ec5cce5a87be107",
+    "url": "/static/media/5.8f1bbccd.jpg"
+  },
+  {
+    "revision": "666316b16df1559a8e1c640b1a63f226",
+    "url": "/static/media/6.666316b1.jpg"
+  },
+  {
+    "revision": "1238d1023312aeab9cf883ddbc21d294",
+    "url": "/static/media/7.1238d102.jpg"
+  },
+  {
+    "revision": "aaa147e11327989831e455ba65532634",
+    "url": "/static/media/8.aaa147e1.jpg"
+  },
+  {
+    "revision": "b765e7a85274c0da965deaae5e276130",
+    "url": "/static/media/9.b765e7a8.jpg"
+  },
+  {
+    "revision": "61be168329bdb0a0848a0b18b69b333a",
+    "url": "/static/media/Baizhou_Chen_Resume.61be1683.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-web/precache-manifest.5c349d9120ab0c052aa2c3472e8914f0.js"
+  "/precache-manifest.8e05ab0ed97dec540b36b59a3a1a38fa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-web/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
